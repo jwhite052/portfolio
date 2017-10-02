3b772694039878414b4c89a138d1fe4b079a123a
Jashua R. White <jashuawhite@gmail.com>
2017-10-02 13:32:52 -0400

    Update docs

--- static/js/theme-custom.js
+++ static/js/theme-custom.js
@@ -1,76 +1,31 @@
 $(document).ready(function() {
-  var $form = $('#contact-form');
-  var $field_email = $form.find('input[name="_replyto"]');
-  var $field_message = $form.find('textarea[name="message"]');
-  var $form_submit = $form.find('button');
-  var $form_inputs = $form.find('input[name="_replyto"], textarea[name="message"]');
+  $('#contact-form').submit(function(e) {
+      var name = $('#inputName')
+      var email = $('#inputEmail')
+      var message = $('#inputMessage')
 
-  $form.on("keypress", ":input:not(textarea)", function(event) {
-    return event.keyCode != 13;
-  });
-
-  $form_inputs.on({
-    'focusin': function() {
-      $(this).addClass('focus');
-    },
-    'focusout': function() {
-      $(this).removeClass('focus');
-    }
-  });
-
-  $field_email.on({
-    'keyup': function() {
-      console.log("Focus E-mail");
-      if (validateEmail($(this).val())) {
-        console.log("Valid E-mail");
-        $(this).addClass('valid').removeClass('invalid');
-      } else {
-        console.log("Invalid E-mail");
-        $(this).addClass('invalid').removeClass('valid');
+      if(name.val() == "" || email.val() == "" || message.val() == "") {
+        $('.submit-fail').fadeToggle(400);
+        return false;
       }
-    }
-  });
-
-  $field_message.on({
-    'keyup': function() {
-      console.log("Focus Message");
-      if (validateMessage($(this).val())) {
-        console.log("Valid Message - Length = " + $(this).val().length);
-        $(this).addClass('valid').removeClass('invalid');
-      } else {
-        console.log("Invalid Message - Length = " + $(this).val().length);
-        $(this).addClass('invalid').removeClass('valid');
-      }
-    }
-  });
-
-  $form.on({
-    'submit': function(e) {
-      if (validateEmail($field_email.val()) && validateMessage($field_message.val())) {
-        $('.submit-overlay').show();
-        console.log("Message submitted.");
+      else {
         $.ajax({
-          url: "//formspree.io/jashuawhite@gmail.com",
-          method: "POST",
-          data: {
-            message: $field_message
-          },
-          dataType: "json",
+          method: 'POST',
+          url: '//formspree.io/jashuawhite@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json',
           success: function() {
-            console.log("Message successfully sent.");
-            $('.submit-overlay').hide();
+            console.log("Message success!");
           }
         });
         e.preventDefault();
+        $(this).get(0).reset();
+        $('.submit-success').fadeToggle(400);
       }
-    }
-  });
+    });
 
-  function validateEmail(email) {
-    var pattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-    return pattern.test(email) ? true : false;
-  }
-  function validateMessage(message) {
-    return message.length >= 10 ? true : false;
-  }
+  $('.submit-fail, .submit-success').click(function() {
+    $(this).hide();
+  })
 });
+  
